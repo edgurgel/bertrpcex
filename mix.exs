c2f4739142c0ee23e83d74046acbf5cab4d20c2d
Eduardo Gurgel <edgurgel@gmail.com>
2013-08-17 17:41:27 -0300

    Update to 0.10.1 for Enum.shuffle
    Also update exlager

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BertrpcEx.Mixfile do
   def project do
     [ app: :bertrpcex,
       version: "0.0.2",
-      elixir: "~> 0.10.0",
+      elixir: "~> 0.10.1",
       deps: deps(Mix.env) ]
   end
 
